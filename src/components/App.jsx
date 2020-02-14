fe18ce42de3487bb020925d1fdff760b99899504
Student <example@example.com>
2020-02-14 11:16:50 -0800

    continue to build on project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,29 @@ import Header from './Header';
 import BeerList from './BeerList';
 import { Switch, Route } from 'react-router-dom';
 import NewBeerForm from './NewBeerForm';
+import Error404 from './Error404';
+import Background from '../assets/images/background.jpg';
 
 function App(){
+  var style = {  
+    backgroundImage: 'url(' + Background + ')',
+    backgroundPosition: 'fixed',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'repeat',
+    minHeight: '100vh',
+    minWidth: '100%'
+  };
+
   return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={BeerList} />
-        <Route path='/newbeer' component={NewBeerForm} />
-      </Switch>
+    <div style={style}>
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={BeerList} />
+          <Route path='/newbeerform' component={NewBeerForm} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
     </div>
   );
 }
